b15f9402a1700f21341dcca43d1a719723f7fd03
J <kiendo95@gmail.com>
2016-07-18 15:33:04 +0700

    Test params

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', (req, res) => {
-	res.end('Hello Heroku');
+	res.end('Hello Heroku')
+})
+
+app.get('/:time', (req, res) => {
+	var timestring = req.params.time;
+	console.log(timestring);
 })
 
 app.listen(app.get('port'), () => {
